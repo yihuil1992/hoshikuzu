9b8ac6b1cf3fd156eeb89050dc1d535dbdb03830
yihuiAcre <yl2900@nyu.edu>
2023-05-29 08:53:16 -0400

    Add google analytics

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,17 @@ export const metadata = {
 export default function RootLayout({children}) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FSTDX0TTQN"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-FSTDX0TTQN');
+                `,
+        }} />
+      </head>
       <body>
         <Providers>
           {children}
