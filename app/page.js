bbfbb7ecd9b3b00b96c5bda4ef6d453b5d044143
Yihui Liu <yl2900@nyu.edu>
2023-08-11 20:13:51 -0400

    add a screen shot on homepage

--- app/page.js
+++ app/page.js
@@ -1,8 +1,15 @@
 // eslint-disable-next-line require-jsdoc
+import Image from 'next/image';
+
+// eslint-disable-next-line require-jsdoc
 export default function Home() {
   return (
     <div>
       ほしくずへようこそ
+      <Image
+        src={'https://cdn.ototrading.com/image/Screenshot.png'} alt={'screenshot'}
+        width={500}
+        height={500}/>
     </div>
   );
 }
